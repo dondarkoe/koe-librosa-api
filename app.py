b0d646e6a4a05d5e075d0ebcbfb79413fd5e6c43
dondarkoe <quincy.patti@gmail.com>
2025-08-27 21:53:27 +0800

    Change default port from 8080 to 5000

--- app.py
+++ app.py
@@ -118,5 +118,5 @@ def estimate_key(key_profile):
     return best_key
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
